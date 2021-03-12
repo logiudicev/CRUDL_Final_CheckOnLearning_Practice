57b1d7e91276a8abbb292651596abdc9d750933e
Vincent R. LoGiudice <vincent.r.logiudice@gmail.com>
2021-03-12 13:37:48 -0600

    some testy tests

--- src/test/java/Mil/SoftwareFactory/CRUDL/EmployeeTests.java
+++ src/test/java/Mil/SoftwareFactory/CRUDL/EmployeeTests.java
@@ -1,8 +1,5 @@
 package Mil.SoftwareFactory.CRUDL;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import Mil.SoftwareFactory.CRUDL.DAO.EmployeeRepository;
 import Mil.SoftwareFactory.CRUDL.Model.EmployeeEntity;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,9 +12,13 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.transaction.annotation.Transactional;
-import static org.hamcrest.Matchers.is;
+
 import java.sql.Date;
+
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 //import javax.transaction.Transactional;
 
 //import static java.util.Collections.get;
@@ -105,6 +106,69 @@ public class EmployeeTests {
         .andExpect(jsonPath("$.name", is("Cosmo Kramer")))
         .andExpect(jsonPath("$.publishDate", is("2021-03-11")))
         .andExpect(jsonPath("$.id").isNumber());
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void patchTest() throws Exception{
+        //Setup by instantiating a new object and assigning values
+        EmployeeEntity newEmployee = new EmployeeEntity();
+        newEmployee.setName("Cosmo Kramer");
+        newEmployee.setId(1);
+        newEmployee.setPublishDate(Date.valueOf("2021-03-10"));
+
+        ObjectMapper jsonCreator = new ObjectMapper();
+        String json = jsonCreator.writeValueAsString(newEmployee);
+
+
+
+
+        //Create MockHttpServletRequestBuilder
+        MockHttpServletRequestBuilder patchRequest = patch("/employees/1")
+
+        .contentType(MediaType.APPLICATION_JSON)
+
+                //add to request body
+                .content("{\"name\":\"Cosmo Kramer\". \"publishDate\":\"2021-03-11\"}")
+                .content(json);
+
+
+        //Execute via this.mvc.perform
+        mvc.perform(patchRequest)
+        .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is("Cosmo Kramer")))
+                .andExpect(jsonPath("$.publishDate", is("2021-03-10")))
+                .andExpect(jsonPath("$.id").isNumber());
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void delTest () throws Exception {
+        EmployeeEntity newEmployee = new EmployeeEntity();
+        newEmployee.setName("Cosmo Kramer");
+        newEmployee.setId(1);
+        newEmployee.setPublishDate(Date.valueOf("2021-03-10"));
+
+        ObjectMapper jsonCreator = new ObjectMapper();
+        String json = jsonCreator.writeValueAsString(newEmployee);
+        MockHttpServletRequestBuilder postRequest = post ("/employees");
+        MockHttpServletRequestBuilder delRequest = delete("/employees/1")
+        .contentType(MediaType.APPLICATION_JSON)
+
+                //add to request body
+                .content("{\"name\":\"Cosmo Kramer\". \"publishDate\":\"2021-03-11\"}")
+                .content(json);
+
+
+        //Execute via this.mvc.perform
+        this.mvc.perform(postRequest);
+        this.mvc.perform(delRequest)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is("Cosmo Kramer")))
+                .andExpect(jsonPath("$.publishDate", is("2021-03-10")))
+                .andExpect(jsonPath("$.id").isNumber());
     }
 
 }
